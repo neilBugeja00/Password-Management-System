cc020d74756a421ea2429102a445a69a0bdf5e66
neilbugeja00 <neilbugeja2000@gmail.com>
2021-09-18 18:29:29 +0200

    table sort functional as intended

--- viewPassword.php
+++ viewPassword.php
@@ -11,9 +11,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Passwords</title>
     <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="tablesort.css"> -->
 </head>
+
 <body>
-    <table class = "table">
+    <table class = "table table-sortable">
         <thead>
             <tr>
                 <th onclick="sortTable(0)">Website</th>
@@ -40,7 +42,7 @@
                         echo '<td>'. $row['pass_email'] .'</td>';
                         echo '<td>'. $row['pass_created'] .'</td>';
                         echo '<td>'. $row['pass_expiry'] .'</td>';
-                        echo '</tr>';
+                    echo '</tr>';
                 }
                 } else {
                 echo "0 results";
@@ -49,62 +51,8 @@
             ?>
         </tbody>
     </table>
+    <script src="tablesort.js"></script>
 </body>
-</html>
 
-<script>
-    function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable2");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-</script>
+<a href="index.php">Back to homepage</a>
+</html>
